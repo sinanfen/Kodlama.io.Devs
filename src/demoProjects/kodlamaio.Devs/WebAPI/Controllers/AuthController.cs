3f31aea3a4fb06ea54ea301a70feeadfe5d715f7
Sinan <sinanfen@hotmail.com>
2022-10-14 03:45:46 +0300

    authController-register-method

--- src/demoProjects/kodlamaio.Devs/WebAPI/Controllers/AuthController.cs
+++ src/demoProjects/kodlamaio.Devs/WebAPI/Controllers/AuthController.cs
@@ -1,5 +1,9 @@
-﻿using Application.Features.Developers.Commands.CreateDeveloper;
+﻿using Application.Features.Auths.Commands.Register;
+using Application.Features.Auths.Dtos;
+using Application.Features.Developers.Commands.CreateDeveloper;
 using Application.Features.Developers.Commands.LoginDeveloper;
+using Core.Security.Dtos;
+using Core.Security.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +14,23 @@ namespace WebAPI.Controllers
     public class AuthController : BaseController
     {
         [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] CreateDeveloperCommand createDeveloperCommand)
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto)
         {
-            var result = await Mediator.Send(createDeveloperCommand);
-            return Ok(result);
+            RegisterCommand registerCommand = new()
+            {
+                userForRegisterDto = userForRegisterDto,
+                IpAddress = GetIpAddress()
+            };
+
+            RegisteredDto result = await Mediator.Send(registerCommand);
+            SetRefreshTokenToCookie(result.RefreshToken);
+            return Created("", result.AccessToken);
+        }
+
+        private void SetRefreshTokenToCookie(RefreshToken refreshToken)
+        {
+            CookieOptions cookieOptions = new() { HttpOnly = true, Expires = DateTime.Now.AddDays(7) };
+            Response.Cookies.Append("refreshToken", refreshToken.Token, cookieOptions);
         }
 
         [HttpPost("login")]
